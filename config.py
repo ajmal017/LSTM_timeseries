255001cd308adf5a9449ff40ab650ae9fb27ccae
sirdavealot <david.g.saccon@gmail.com>
2018-08-14 04:10:35 +0000

    added new files

---
+++ config.py
@@ -0,0 +1,14 @@
+CONFIG = {
+    'pair': '',
+    'period': 300,
+    'input_size': 96,
+    'output_size': 4,
+    'lstm_hidden_size': 50,
+    'columns' : '',
+    'csv_src_file' : '',
+    'name': 'lstm',
+    'folder': {
+        'data': 'data/',
+        'weights': 'weights/',
+        }
+}
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 from pandas import DataFrame
 from pandas import concat
+import pandas as pd
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
 
 # This converts a time series into an inputs and outpus for training
 # Learn more at:
@@ -16,7 +26,7 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 		Pandas DataFrame of series framed for supervised learning.
 	"""
 	n_vars = 1 if type(data) is list else data.shape[1]
-	df = DataFrame(data)
+	df = pd.DataFrame(data)
 	cols, names = list(), list()
 	# input sequence (t-n, ... t-1)
 	for i in range(n_in, 0, -1):
@@ -30,9 +40,136 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 		else:
 			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
 	# put it all together
-	agg = concat(cols, axis=1)
+	agg = pd.concat(cols, axis=1)
 	agg.columns = names
 	# drop rows with NaN values
 	if dropnan:
 		agg.dropna(inplace=True)
 	return agg
+
+def create_dataset():
+    df = pd.read_json("eth-donation-requests.json")
+    df['tagged_begging'] = pd.to_datetime(df['tagged_begging'])
+    df = df.set_index(pd.DatetimeIndex(df['tagged_begging']))
+    df = df.groupby(pd.Grouper(key='tagged_begging', freq='15Min', base=0, label='right'))
+    df = df.describe()
+    return df.unstack()['tagged_begging']['count']
+
+# create a differenced series
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return pd.Series(diff)
+
+# transform series into train and test sets for supervised learning
+def prepare_data(series, n_test, n_lag, n_seq):
+	# extract raw values
+	raw_values = series.values
+	# transform data to be stationary
+	diff_series = difference(raw_values, 1)
+	diff_values = diff_series.values
+	diff_values = diff_values.reshape(len(diff_values), 1)
+	# rescale values to -1, 1
+	scaler = MinMaxScaler(feature_range=(-1, 1))
+	scaled_values = scaler.fit_transform(diff_values)
+	scaled_values = scaled_values.reshape(len(scaled_values), 1)
+	# transform into supervised learning problem X, y
+	supervised = series_to_supervised(scaled_values, n_lag, n_seq)
+	supervised_values = supervised.values
+	# split into train and test sets
+	train, test = supervised_values[0:-n_test], supervised_values[-n_test:]
+	return scaler, train, test
+
+# fit an LSTM network to training data
+def fit_lstm(train, n_lag, n_seq, n_batch, nb_epoch, n_neurons):
+	# reshape training into [samples, timesteps, features]
+	X, y = train[:, 0:n_lag], train[:, n_lag:]
+	X = X.reshape(X.shape[0], 1, X.shape[1])
+	# design network
+	model = Sequential()
+	model.add(LSTM(n_neurons, batch_input_shape=(n_batch, X.shape[1], X.shape[2]), stateful=True))
+	model.add(Dense(y.shape[1]))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	# fit network
+	for i in range(nb_epoch):
+		model.fit(X, y, epochs=1, batch_size=n_batch, verbose=1, shuffle=False)
+		model.reset_states()
+	return model
+
+# make one forecast with an LSTM,
+def forecast_lstm(model, X, n_batch):
+	# reshape input pattern to [samples, timesteps, features]
+	X = X.reshape(1, 1, len(X))
+	# make forecast
+	forecast = model.predict(X, batch_size=n_batch)
+	# convert to array
+	return [x for x in forecast[0, :]]
+
+# make a persistence forecast
+def persistence(last_ob, n_seq):
+	return [last_ob for i in range(n_seq)]
+
+# evaluate the persistence model
+def make_forecasts(model, n_batch, train, test, n_lag, n_seq):
+	forecasts = list()
+	for i in range(len(test)):
+		X, y = test[i, 0:n_lag], test[i, n_lag:]
+		# make forecast
+		forecast = forecast_lstm(model, X, n_batch)
+		# store the forecast
+		forecasts.append(forecast)
+	return forecasts
+
+# evaluate the RMSE for each forecast time step
+def evaluate_forecasts(test, forecasts, n_lag, n_seq):
+	for i in range(n_seq):
+		actual = [row[i] for row in test]
+		predicted = [forecast[i] for forecast in forecasts]
+		rmse = sqrt(mean_squared_error(actual, predicted))
+		print('t+%d RMSE: %f' % ((i+1), rmse))
+	r2 = r2_score(test, forecasts)
+	print("R2:", r2)
+
+# invert differenced forecast
+def inverse_difference(last_ob, forecast):
+	# invert first forecast
+	inverted = list()
+	inverted.append(forecast[0] + last_ob)
+	# propagate difference forecast using inverted first value
+	for i in range(1, len(forecast)):
+		inverted.append(forecast[i] + inverted[i-1])
+	return inverted
+
+# inverse data transform on forecasts
+def inverse_transform(series, forecasts, scaler, n_test):
+	inverted = list()
+	for i in range(len(forecasts)):
+		# create array from forecast
+		forecast = np.array(forecasts[i])
+		forecast = forecast.reshape(1, len(forecast))
+		# invert scaling
+		inv_scale = scaler.inverse_transform(forecast)
+		inv_scale = inv_scale[0, :]
+		# invert differencing
+		index = len(series) - n_test + i - 1
+		last_ob = series.values[index]
+		inv_diff = inverse_difference(last_ob, inv_scale)
+		# store
+		inverted.append(inv_diff)
+	return inverted
+
+# plot the forecasts in the context of the original dataset
+def plot_forecasts(series, forecasts, n_test):
+	# plot the entire dataset in blue
+	plt.plot(series.values)
+	# plot the forecasts in red
+	for i in range(len(forecasts)):
+		off_s = len(series) - n_test + i - 1
+		off_e = off_s + len(forecasts[i]) + 1
+		xaxis = [x for x in range(off_s, off_e)]
+		yaxis = [series.values[off_s]] + forecasts[i]
+		plt.plot(xaxis, yaxis, color='red')
+	# show the plot
+	plt.show()
